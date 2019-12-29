bef7b53aac31d29aead1c9f8973cfa701df6f409
Hugo Josefson <hugo@josefson.org>
2019-12-29 17:50:51 +0000

    Refactor sapper-cors.

--- src/server/app.js
+++ src/server/app.js
@@ -7,20 +7,25 @@ import { manifest } from '@sapper/internal/manifest-server'
 import initAppState from './app-state'
 import populateReq from '../middleware/populate-req'
 import populateDomain from '../middleware/populate-domain'
-import populateDeclaredRoute from '../middleware/populate-declared-route'
+import populateSapperRoute from '../middleware/populate-sapper-route'
 import sapperCors from '../middleware/sapper-cors'
 import errorHandler from '../middleware/error-handler'
 
 export default async ({ dev }) => {
   const { getPath } = await initAppState()
+  const reqProp = 'sapperRoute'
+  const corsOptions = { origin: true }
 
   return express()
     .use(compression({ threshold: 0 }))
     .use(sirv('static', { dev }))
-    .use(populateDeclaredRoute(manifest))
-    .use(sapperCors({ origin: true }))
+
+    .use(populateSapperRoute({ manifest, reqProp }))
+    .use(sapperCors({ corsOptions, reqProp }))
+
     .use('/api/**', populateReq({ getPath }))
     .use('/api/domains/:domain', populateDomain(getPath))
     .use(errorHandler({ dev }))
+
     .use(sapper.middleware())
 }
