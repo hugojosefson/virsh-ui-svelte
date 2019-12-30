7ad605e1cdc58963430190c13073ca19ecf8d159
Hugo Josefson <hugo@josefson.org>
2019-12-30 11:28:30 +0000

    Fix circular dependency.

--- src/routes/api/domains/[domainId]/index.js
+++ src/routes/api/domains/[domainId]/index.js
@@ -1,23 +1,19 @@
 import wrapInErrorHandler from '../../../_wrap-in-error-handler'
 import s from '../../../../fn/s'
+import { selfLink as domainsLink } from '../_self-link'
 
-export const render = ({ protocol, headers, domain }) => ({
-  _links: renderLinks({ protocol, headers, domain }),
-  ...domain
+export const render = req => ({
+  _links: renderLinks(req),
+  ...req.domain
 })
 
-export const renderLinks = ({ protocol, headers, domain }) => ({
-  self: selfLink({ protocol, domain, headers }),
-  start: mayStart(domain)
-    ? `${selfLink({ protocol, domain, headers })}/start`
-    : undefined,
-  shutdown: mayShutdown(domain)
-    ? `${selfLink({ protocol, domain, headers })}/shutdown`
-    : undefined
+export const renderLinks = req => ({
+  self: selfLink(req),
+  start: mayStart(req.domain) ? `${selfLink(req)}/start` : undefined,
+  shutdown: mayShutdown(req.domain) ? `${selfLink(req)}/shutdown` : undefined
 })
 
-export const selfLink = ({ protocol, headers: { host }, domain: { id } }) =>
-  `${protocol}://${host}/api/domains/${id}`
+export const selfLink = req => `${domainsLink(req)}/${req.domain.id}`
 
 const mayStart = ({ state }) =>
   !['running', 'started'].includes(state.toLowerCase())
