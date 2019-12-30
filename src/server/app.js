c369f69949a98b50737668920577364ee4aeff28
Hugo Josefson <hugo@josefson.org>
2019-12-30 09:47:42 +0000

    Rename reqProp to routeProp.

--- src/server/app.js
+++ src/server/app.js
@@ -13,7 +13,7 @@ import errorHandler from '../middleware/error-handler'
 
 export default async ({ dev, trustProxy }) => {
   const { getPath } = await initAppState()
-  const reqProp = 'sapperRoute'
+  const routeProp = 'sapperRoute'
   const corsOptions = { origin: true }
 
   return express()
@@ -21,8 +21,8 @@ export default async ({ dev, trustProxy }) => {
     .use(compression({ threshold: 0 }))
     .use(sirv('static', { dev }))
 
-    .use(populateSapperRoute({ manifest, reqProp }))
-    .use(sapperCors({ corsOptions, reqProp }))
+    .use(populateSapperRoute({ manifest, routeProp }))
+    .use(sapperCors({ corsOptions, routeProp }))
 
     .use('/api/**', populateReq({ getPath }))
     .use('/api/domains/:domain', populateDomain(getPath))
