c0a4fcf0595d9765a30b04c0aca12b307a5c291a
Hugo Josefson <hugo@josefson.org>
2019-12-30 10:27:43 +0000

    Implement client to browse, view domains.

---
+++ src/routes/domains/[domainId].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export async function preload(page, session) {
+    const {domainId} = page.params
+    const domain = await this.fetch(`/api/domains/${domainId}`).then(res => res.json())
+    return { domain }
+  }
+</script>
+
+<script>
+  export let domain
+</script>
+
+<a href="/domains">&lt;-- back</a>
+
+<h1>/domains/{domain.data.attributes.name}</h1>
+
+<code><pre>{JSON.stringify(domain, null, 2)}</pre></code>
---
+++ src/routes/domains/index.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+  export async function preload(page, session) {
+    const domains = await this.fetch('/api/domains').then(res => res.json())
+    return { domains }
+  }
+</script>
+
+<script>
+  export let domains
+</script>
+
+<a href="/">&lt;-- back</a>
+
+<h1>/domains</h1>
+
+<ul>
+  {#each domains.data as domain}
+    <li>
+      [ {domain.attributes.state} ] &nbsp;
+      <a href={`/domains/${domain.id}`}>{domain.attributes.name}</a>
+    </li>
+  {/each}
+</ul>
