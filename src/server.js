f947692194b6ad16fb046e79025c33ddd10a2d7a
Hugo Josefson <hugo@josefson.org>
2020-05-04 20:41:09 +0000

    Support TRUST_PROXY env var.

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ import compression from 'compression'
 import * as sapper from '@sapper/server'
 import envConfig from '@hugojosefson/env-config'
 
-const { PORT, NODE_ENV } = envConfig()
+const { PORT, NODE_ENV, TRUST_PROXY } = envConfig()
 const dev = NODE_ENV === 'development'
+const trustProxy =
+  typeof TRUST_PROXY === 'undefined' ? () => false : TRUST_PROXY
 
 express()
   .use(
@@ -13,6 +15,7 @@ express()
     sirv('static', { dev }),
     sapper.middleware()
   )
+  .set('trust proxy', trustProxy)
   .listen(PORT, err => {
     if (err) console.error('error', err)
   })
