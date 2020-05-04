8eeba01df73845349d392d2c46b5624988e38ac8
Hugo Josefson <hugo@josefson.org>
2020-05-04 23:04:11 +0200

    Use @hugojosefson/env-config,

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ import sirv from 'sirv'
 import express from 'express'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
+import envConfig from '@hugojosefson/env-config'
 
-const { PORT, NODE_ENV } = process.env
+const { PORT, NODE_ENV } = envConfig()
 const dev = NODE_ENV === 'development'
 
 express()
